3840cbb1df313d8555aca3cf6ce3e7c92fd3fa20
IgnacioMorenoJ <ignaciomj23@gmail.com>
2021-05-19 22:26:28 -0500

    Consultado datos de la tabla usuario

---
+++ ProyectoTrimestre3Asp/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoTrimestre3Asp.Models;
+
+namespace ProyectoTrimestre3Asp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+                       
+        {
+            using (var db = new inventario2021Entities())
+
+            {
+               return View(db.usuarios.ToList());
+
+            }
+
+               
+        }
+    }
+}
