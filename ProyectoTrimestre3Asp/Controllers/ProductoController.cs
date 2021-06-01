5cb583e1ae28c4d958a4aa3cf856139877d72f53
IgnacioMorenoJ <ignaciomj23@gmail.com>
2021-05-31 19:09:54 -0500

    CRUD Producto

--- ProyectoTrimestre3Asp/Controllers/ProductoController.cs
+++ ProyectoTrimestre3Asp/Controllers/ProductoController.cs
@@ -61,5 +61,91 @@ namespace ProyectoTrimestre3Asp.Controllers
                 return View();
             }
         }
-    }  
- }
+
+        public ActionResult Details(int id)
+
+        {
+            using (var db = new inventario2021Entities())
+
+            {
+                producto productoDetalle = db.productoes.Where(a => a.id == id).FirstOrDefault();
+                return View(productoDetalle);
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            using (var db = new inventario2021Entities())
+            {
+                var productDelete = db.productoes.Find(id);
+                db.productoes.Remove(productDelete);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Edit(int id)
+
+        {
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+
+                {
+                    producto producto = db.productoes.Where(a => a.id == id).FirstOrDefault();
+                    return View(producto);
+                }
+
+            } catch (Exception ex)
+
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+
+            }
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit (producto productoEdit) 
+        
+        { 
+            try 
+            
+            { 
+
+             using ( var db = new inventario2021Entities())
+                
+             {
+                    var producto = db.productoes.Find(productoEdit.id);
+                    producto.nombre = productoEdit.nombre;
+                    producto.percio_unitario = productoEdit.percio_unitario;
+                    producto.cantidad = productoEdit.cantidad;
+                    producto.descripcion = productoEdit.descripcion;
+                    producto.id_proveedor = productoEdit.id_proveedor;
+                    
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                     
+             }
+
+            } catch (Exception ex)
+            
+            { 
+                ModelState.AddModelError ("", "error" + ex);
+                return View();
+            }
+
+        }
+
+
+    }
+
+    
+}
