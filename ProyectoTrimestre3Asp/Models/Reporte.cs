57af67c6533b6d9bef4d0aa0fddff3e14eb21498
IgnacioMorenoJ <ignaciomj23@gmail.com>
2021-06-18 19:13:46 -0500

    M

---
+++ ProyectoTrimestre3Asp/Models/Reporte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTrimestre3Asp.Models
+{
+    public class Reporte
+    {
+        public String NombreProveedor { get; set; }
+        public String DireccionProveedor { get; set; }
+        public String TelefonoProveedor { get; set; }
+        public String NombreProducto { get; set; }
+        public int PrecioProducto { get; set; }
+
+    }
+}
